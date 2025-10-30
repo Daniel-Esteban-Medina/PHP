32a735d3f0e73d5edaeb41c9ebc2563b9bd000c3
diurno <diurno@B27-B3.lan.tierno.es>
2025-10-30 14:27:30 +0100

    Telefonos

--- ejerciciosU4POO/clases/Empleado.php
+++ ejerciciosU4POO/clases/Empleado.php
@@ -3,6 +3,7 @@ class Empleado{
     public $nombre;
     public $apellidos;
     public $sueldo;
+    public $telefonos = [];
 
     public function __construct($nombre, $apellidos, $sueldo = -1){
         $this->nombre = $nombre;
@@ -70,5 +71,26 @@ class Empleado{
             $re = -1;
         }
         return $re;
+    }
+    public function addTelefono($num){
+        $this->telefonos[] = $num;
+    }
+    public function listarTelefonos():string{
+        $lista = "";
+        for($i = 0; $i < count($this->telefonos); $i++){
+            if($i < count($this->telefonos) - 1){
+                $lista = $lista.$this->telefonos[$i].", ";
+            } else {
+                $lista = $lista.$this->telefonos[$i];
+            }
+        }
+        return $lista;
+    }
+    public function eliminarTelefonos(){
+        $this->telefonos = [];
+    }
+    public function toHtml():string{
+        $html = "<p>nombre: $this->nombre apellidos: $this->apellidos salario: </p>";
+        return $html;
     }
 }
