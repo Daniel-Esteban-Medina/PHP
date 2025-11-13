62bc5f1c94fdab1e8767075c943c61a64fc2ea40
diurno <diurno@B27-B3.lan.tierno.es>
2025-11-13 14:15:46 +0100

    PHPDOC implementado

--- POO_DanielE/utils/funcionesGenerales.php
+++ POO_DanielE/utils/funcionesGenerales.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Genera una lista ordenada (HTML) a partir de un array.
+ *
+ * @param array $arr Lista de elementos a mostrar en la lista ordenada.
+ * @return string Código HTML que representa la lista ordenada (<ol>).
+ */
 function arrayListOrder($arr){
     $html = "<ol>";
     for($i= 0;$i<count($arr);$i++){
@@ -7,6 +13,12 @@ function arrayListOrder($arr){
     $html .= "</ol>"; 
     return $html;
 }
+/**
+ * Genera una lista desordenada (HTML) a partir de un array de elementos.
+ *
+ * @param array $arr Lista de elementos a mostrar en la lista desordenada.
+ * @return string Código HTML que representa la lista desordenada (<ul>).
+ */
 function arrayListUnorder($arr){
     $html = "<ul>";
     for($i= 0;$i<count($arr);$i++){
